a37982ef7302d456d3deb07b9613d908f67b9886
Muna Rai <raimuna669@gmailcom>
2022-03-16 14:18:53 +0545

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibrarianComponent } from './librarian/librarian.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'librarian-component', component: LibrarianComponent},
+  {path: 'students-component', component: StudentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
